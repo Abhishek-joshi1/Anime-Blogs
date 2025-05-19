226a01bfa279c07f4a1a86755c8252fae1cf5cde
Abhishek-joshi1 <abhisbekjoshi@gmail.com>
2025-05-18 21:43:57 -0700

    configure the post form and RTE

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,11 @@ import Footer from "./Footer/Footer"
 import Container from "../container/Container"
 import Logo from "./logo/Logo"
 import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE"
+import Input from "./Input"
+import Button from "./Button"
+import Select from "./Select"
 
 export {
-    Header, Footer, Container, Logo, LogoutBtn
+    Header, Footer, Container, Logo, LogoutBtn, RTE, Input, Button, Select
 }
