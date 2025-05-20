8c975d4b17ce0fa678dbfacad41363a5c9620eee
Abhishek-joshi1 <abhisbekjoshi@gmail.com>
2025-05-20 05:13:30 -0700

    Form Here we change only the styling section

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   }, [])
   
   return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='max-w-screen-xl mx-auto min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
         <Header />
         <main>
